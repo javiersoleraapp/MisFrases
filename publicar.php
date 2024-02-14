2208c0a5621c3ff556501d2efc2fd570803bb638
Javier Solera <javier.solera@endalia.com>
2024-02-14 22:10:36 +0100

    Guardar IP origen

--- publicar.php
+++ publicar.php
@@ -2,18 +2,23 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Obtén la frase y la dirección IP
     $frase = $_POST["frase"];
+    $ip = $_SERVER["REMOTE_ADDR"];
+
+    // Formatea la fecha actual
     $fecha = date("Y-m-d H:i:s");
 
-    // Obtener el nombre de usuario o establecerlo como "Anónimo" si no está autenticado
-    $usuario = isset($_SESSION['username']) ? $_SESSION['username'] : "Anónimo";
+    // Almacena la frase, la dirección IP y la fecha en un archivo
+    $mensaje = "$fecha | $frase | $ip\n";
+    file_put_contents("_mensajes.txt", $mensaje, FILE_APPEND);
 
-    $registro = "$fecha - $frase - $usuario\n";
-
-    // Guardar la frase en un archivo de texto
-    file_put_contents("frases.txt", $registro, FILE_APPEND);
+    // Redirige a la página principal
+    header("Location: index.php");
+    exit;
+} else {
+    // Si se intenta acceder directamente a este archivo sin un formulario POST, redirige a la página principal
+    header("Location: index.php");
+    exit;
 }
-
-header("Location: index.php");
-exit;
 ?>
